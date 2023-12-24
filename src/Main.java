6d33649eee94f47de1ba67d2975e8bdea532761a
Kirill <mywo960@gmail.com>
2023-12-24 16:37:17 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Введите MAC-адрес: ");
+        String inputMac = scanner.nextLine();
+
+        if (isMac(inputMac)) {
+            System.out.println("Введенный MAC-адрес является правильным.");
+        } else {
+            System.out.println("Введенный MAC-адрес не является правильным.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isMac(String mac) {
+
+        String macPattern = "^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$";
+
+
+        Pattern pattern = Pattern.compile(macPattern);
+
+
+        Matcher matcher = pattern.matcher(mac);
+
+
+        return matcher.matches();
+    }
+}
